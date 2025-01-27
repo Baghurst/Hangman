291d81374dac5c1eb2dc390ab9872ffe22650f60
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-27 13:43:42 +0000

    Add files via upload

---
+++ Hangman Exercise.py
@@ -0,0 +1,52 @@
+#Hangman Exercise
+#The player should get 7 lives
+#You need one string to hold the secret word
+#You need one string of blank spaces or underscores to hold the valid characters as they are entered
+word = (input("Random word: "))
+print('_'*len(word))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
