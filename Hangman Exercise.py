89f38d0bf316fdbd68a392276471d5790ba3b808
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-28 01:22:00 +0000

    Update Hangman Exercise.py

--- Hangman Exercise.py
+++ Hangman Exercise.py
@@ -4,8 +4,7 @@
 #You need one string of blank spaces or underscores to hold the valid characters as they are entered
 word = (input("Random word: "))
 print('_'*len(word))
-
-
+guess = input("Guess a letter: ").lower()
 
 
 
