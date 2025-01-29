4edd5acdbfeb1c754ff43671670dc5012958c152
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-29 11:59:18 +0000

    Update Hangman Exercise.py

--- Hangman Exercise.py
+++ Hangman Exercise.py
@@ -3,11 +3,13 @@
 #You need one string to hold the secret word
 #You need one string of blank spaces or underscores to hold the valid characters as they are entered
 
-word = "Monday"
+word = "monday"
 lifes = 7
+entered_letters = ""
 #print(word[0], word[5])
 print('_'*len(word))
 guess = input("Guess a letter: ").lower()
+entered_letters = entered_letters + guess
 while lifes > 0:
     if guess in word:
         print(f"Well done! {guess} is in the word!")
@@ -16,14 +18,16 @@ while lifes > 0:
         print(f"Sorry, {guess} is not in the word, try again!")
         lifes =  lifes - 1
         print(f"You have {lifes} lifes left!")
-    #if lifes == 0:
-        #print(f"GAME OVER! Your have {lifes} lifes left!")
     guess = input("Guess a letter: ").lower()
-else:
-    ("GAME OVER! You have 0 lifes left!")
+    entered_letters = entered_letters + guess  
+print("GAME OVER! You have 0 lifes left!")
     
     
-    
+
+
+
+
+
 
 
 
