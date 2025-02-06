0c06b1d44cdfc2be20cca3d1a729499442719728
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-02-06 11:53:45 +0000

    Update Hangman Exercise.py

--- Hangman Exercise.py
+++ Hangman Exercise.py
@@ -20,42 +20,17 @@ while lifes > 0:
         print(f"Sorry, {guess} is not in the word, try again!")
         lifes =  lifes - 1
         print(f"You have {lifes} lifes left!")
+    if word == success:
+        print("Congratulations! YOU WON!!!")
+        break
     guess = input("Guess a letter: ").lower()
-    entered_letters = entered_letters + guess  
-print("GAME OVER! You have 0 lifes left!")
+    while guess in entered_letters:
+       guess = input("Guess a letter: ").lower() 
+    entered_letters = entered_letters + guess
+else:
+    print("GAME OVER! You have 0 lifes left!")
     
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
